d9fa656f3e0c7351887dc446328a25acc2368a31
Maxim <dovbnenko_ms@mail.ru>
2018-01-08 15:18:56 +0300

    End

--- lesson3.js
+++ lesson3.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
     EditMode = new Edit();
 });
 
-/*-------------------------------------------------------------*/
 function Point2D(){
     this.x = 0;
     this.y = 0;
@@ -24,10 +23,6 @@ Point2D.prototype = {
         this.x = _x; this.y = _y;
     }
 };
-/*-------------------------------------------------------------*/
-/*-------------------------------------------------------------*/
-
-/*-------------------------------------------------------------*/
 function MatrixGroup(){
     this.MaxRow = 25;
     this.MaxCol = 25;
@@ -77,7 +72,6 @@ MatrixGroup.prototype = {
                 }
                 for(var i = 0; i < 5; i++)
                     this.genirate_step();
-                //this.calculateGroup();
                 return true;
             }
         }
@@ -88,8 +82,8 @@ MatrixGroup.prototype = {
         for(var i = 1; i < this.row_length - 1; i++){
             for(var j = 1; j < this.col_length - 1; j++){
                 var current_type = this.Matrix[i][j];
-                for(var dir_i = -1; dir_i < 2; dir_i++){
-                    for(var dir_j = -1; dir_j < 2; dir_j++){
+                for(var dir_i = -1; dir_i <= 1; dir_i++){
+                    for(var dir_j = -1; dir_j <= 1; dir_j++){
                         if(dir_i != 0 && dir_j != 0){
                             var state = this.Matrix[i + dir_i][j + dir_j];
                             if(current_type == this.water && state != this.water){
@@ -116,9 +110,7 @@ MatrixGroup.prototype = {
     loadDefault: function(matrix){
         canvas_elem.height = this.row_length * 24;
         canvas_elem.width  = this.col_length * 24;
-        
         this.Matrix = matrix;
-        //this.calculateGroup();
     },
     drawMatrix: function(){
         for(var i = 0; i < this.row_length; i++){
@@ -228,14 +220,12 @@ Edit.prototype = {
         $('#tail-green').on('click', function(){$this.CurrentColor = 4;});
         $('#next-btn').on('click', function(){ $this.CreateEndMatrix();});
         $('#create-btn').on('click', function(){ $this.CreateStartMatrix();});
-        $('#price-geo-btn').on('click', function(){
-            $this.CreatePriceMatrix();
-        });
+        $('#price-geo-btn').on('click', function(){$this.CreatePriceMatrix();});
+        $('#start-game-btn').on('click', function(){ $(".map").hide(1000);   $('#title-step').hide(); });
         $('#draw_obj').on('mousedown', function(e){
             var m = mouse_coords(e);
             var i = Math.floor((m.x) / 24) ;       
             var j = Math.floor((m.y) / 24) ;   
-            //Canvas.fillStyle  = $this.StartMatrix.cell_color[$this.CurrentColor]; 
             if($this.CurrentCreate == 'start'){
                 $this.StartMatrix.Matrix[j][i] =  $this.CurrentColor; 
                 $this.StartMatrix.drawMatrix();
@@ -346,6 +336,15 @@ Edit.prototype = {
         t_body.appendTo($('#count-group-tb'));
     }
 }
+
+
+function Game(){
+
+}
+Game.prototype = {
+
+};
+
 
 function mouse_coords(e) {                  // функция возвращает экранные координаты курсора мыши
     var m = [];
